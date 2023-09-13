d6d82309839222531acb7f7f77e2383c4040e40c
msu32820223281506121 <144201390+msu32820223281506121@users.noreply.github.com>
2023-09-13 16:40:21 +0530

    Add files via upload

---
+++ unit 2/main (2).py
@@ -0,0 +1,16 @@
+#player
+class Player:
+ def play(self):
+  print("The player is playing cricket.")
+class Batsman(Player):
+ def play(self):
+  print("The batsman is batting.")
+class Bowler(Player):
+ def play(self):
+   print("The bowler is bowling.")
+player=Player()
+player.play()
+batsman = Batsman()
+batsman.play()
+bowler=Bowler() 
+bowler.play()
